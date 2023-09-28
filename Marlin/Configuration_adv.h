8cf9e5c48c7d9130c4dd5945526b1e650f0591e7
messengermakes <146210691+messengermakes@users.noreply.github.com>
2023-09-28 17:28:25 +0100

    V1 Basic config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3180,8 +3180,7 @@
    * Use Trinamic's ultra quiet stepping mode.
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
-  #if HAS_STEALTHCHOP
-    #define STEALTHCHOP_XY
+      #define STEALTHCHOP_XY
     #define STEALTHCHOP_Z
     #define STEALTHCHOP_I
     #define STEALTHCHOP_J
